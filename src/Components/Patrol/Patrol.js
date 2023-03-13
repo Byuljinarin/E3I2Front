6a56ead41eebfe04e0711eff97c9b0d161cc61b8
SeonWoong Yang <byuljinarin@gmail.com>
2023-03-13 09:51:55 +0900

    patrol css 수정

--- src/Components/Patrol/Patrol.js
+++ src/Components/Patrol/Patrol.js
@@ -2,15 +2,11 @@ import React from 'react';
 import styles from './Patrol.module.css';
 
 function Patrol(){
-
-
     return <div>
-        <div className={styles.PatrolSection}>
-            <div>
+        <div className={styles.LineSection}>
+            <div className={styles.PatrolSection}>
                 <div className={styles.PatrolVodSection}>동영상 재생될 부분</div>
-                <div className={styles.PatrolInfoSection}>나올정보: 날짜, 마피아, 도로정보
-                밑에서 부터 하나씩 출력
-                </div>
+                <div className={styles.PatrolInfoSection}>나올정보: 날짜, 마피아, 도로정보 밑에서 부터 하나씩 출력</div>
             </div>
         </div>
     </div>
