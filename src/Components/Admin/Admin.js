b838d002dcb6276a55dad1c13aa623647d8479b6
byuljinarin <byuljinarin@gmail.com>
2023-03-14 14:05:54 +0900

    03-14 14:05 어드민 수정

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -5,10 +5,21 @@ import styles from './Admin.module.css';
 function Admin(){
 
     return <div>
-        <div className={styles.AdminSection}>관리자 페이지</div>
-
-        
-
+        <div className={styles.LineSection}>
+            <div className={styles.PatrolSection}>
+                <div className={styles.TitleSection}>관리자 페이지</div>
+            <table className={styles.Table}>      
+                <thead>
+                    <tr>
+                        <th className={styles.TableTitleSection}>관리자 추가</th>
+                        <th className={styles.TableTitleSection}>회원 관리자 지정</th>
+                        <th className={styles.TableTitleSection}>일반 회원 지정</th>
+                        <th className={styles.TableTitleSection}>회원 목록 조회</th>
+                    </tr>
+                </thead>
+            </table>
+            </div>
+        </div>
     </div>
 };
 
